8fbbe36635dfa2115237428bc943b319e6e6cdf3
Errance <errance.liu@gmail.com>
2025-12-08 13:58:11 +0800

    fix: Crowdfund 卡片标题改为 'CDNB Crowdfund'

--- shared/pages/Crowdfund/index.jsx
+++ shared/pages/Crowdfund/index.jsx
@@ -199,8 +199,7 @@ const Crowdfund = ({ profile, crowdfund, exchangePhase, authActions, crowdfundAc
         {/* 卡片头部 */}
         <div className={styles.cardHeader}>
           <div className={styles.cardTitle}>
-            {/* 单交易所模式：隐藏 exchange 名称 */}
-            Crowdfund
+            CDNB Crowdfund
           </div>
           <div className={classNames(styles.statusBadge, {
             [styles.live]: isLive,
